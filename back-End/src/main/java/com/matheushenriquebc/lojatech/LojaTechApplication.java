97479e83fc7d5bc490617245144388bda98118f0
Matheus Henrique <matheushenrique.dev@gmail.com>
2018-05-05 15:16:04 -0300

    projeto adicionado

---
+++ back-End/src/main/java/com/matheushenriquebc/lojatech/LojaTechApplication.java
@@ -0,0 +1,12 @@
+package com.matheushenriquebc.lojatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTechApplication.class, args);
+	}
+}
